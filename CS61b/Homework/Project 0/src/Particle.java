232d07c63c1bf2dbf261bc59e08e565a073bc9b1
KCDC <kcdcunderscore0@gmail.com>
2026-06-06 08:56:38 +0800

    Day 52

--- CS61b/Homework/Project 0/src/Particle.java
+++ CS61b/Homework/Project 0/src/Particle.java
@@ -22,19 +22,65 @@ public class Particle {
     }
 
     public Color color() {
-        if (flavor == ParticleFlavor.EMPTY) {
-            return Color.BLACK;
+        switch (this.flavor) {
+            case EMPTY:
+                return Color.BLACK;
+            case SAND:
+                return Color.YELLOW;
+            case BARRIER:
+                return Color.GRAY;
+            case WATER:
+                return Color.BLUE;
+            case FOUNTAIN:
+                return Color.CYAN;
+            case PLANT:
+                return new Color(0, 255, 0);
+            case FIRE:
+                return new Color(255, 0, 0);
+            case FLOWER:
+                return new Color(255, 141, 161);
+            default:
+                return Color.BLACK;
         }
-        return Color.GRAY;
     }
 
     public void moveInto(Particle other) {
+        if (other == null) {
+            return;
+        }
+
+        other.flavor = this.flavor;
+        other.lifespan = this.lifespan;
+
+        this.flavor = ParticleFlavor.EMPTY;
+        this.lifespan = -1;
     }
 
     public void fall(Map<Direction, Particle> neighbors) {
+        Particle downNeighbor = neighbors.get(Direction.DOWN);
+        if (downNeighbor != null && downNeighbor.flavor == ParticleFlavor.EMPTY) {
+            this.moveInto(downNeighbor);
+        }
     }
 
     public void flow(Map<Direction, Particle> neighbors) {
+        Particle leftNeighbor = neighbors.get(Direction.LEFT);
+        Particle rightNeighbor = neighbors.get(Direction.RIGHT);
+        
+        boolean canMoveLeft = (leftNeighbor != null && leftNeighbor.flavor == ParticleFlavor.EMPTY);
+        boolean canMoveRight = (rightNeighbor != null && rightNeighbor.flavor == ParticleFlavor.EMPTY);
+
+        if (canMoveLeft && canMoveRight) {
+            if (Math.random() < 0.5) {
+                this.moveInto(leftNeighbor);
+            } else {
+                this.moveInto(rightNeighbor);
+            }
+        } else if (canMoveLeft) {
+            this.moveInto(leftNeighbor);
+        } else if (canMoveRight) {
+            this.moveInto(rightNeighbor);
+        }
     }
 
     public void grow(Map<Direction, Particle> neighbors) {
@@ -44,5 +90,16 @@ public class Particle {
     }
 
     public void action(Map<Direction, Particle> neighbors) {
+        if (this.flavor == ParticleFlavor.EMPTY) {
+            return;
+        }
+
+        if (this.flavor != ParticleFlavor.BARRIER) {
+            this.fall(neighbors);
+        }
+
+        if (this.flavor == ParticleFlavor.WATER) {
+            this.flow(neighbors);
+        }
     }
 }
